2a160c6c2b79ac58b274d33e4c50371ffce49d4c
tangbodi <tangbodi@gmail.com>
2023-08-02 08:28:54 -0400

    8/2

--- src/main/java/com/example/demo/Controller/TestController.java
+++ src/main/java/com/example/demo/Controller/TestController.java
@@ -1,7 +1,9 @@
 package com.example.demo.Controller;
 
+import com.example.demo.Util.ApiResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     private static final Logger logger = LoggerFactory.getLogger(TestController.class);
     @GetMapping("/getData")
-    public String getData(){
-        return "Hello World";
+    public ResponseEntity getData(){
+        ApiResponse apiResponse = ApiResponse.success("Hello World");
+        return ResponseEntity.ok(apiResponse);
     }
 
 }
