08ded30246e3fe65e7853322fb5cc4d0da9dd133
tangbodi <tangbodi@gmail.com>
2023-08-27 21:20:58 -0400

    8/27

--- src/main/java/com/example/demo/Enum/StatusEnum.java
+++ src/main/java/com/example/demo/Enum/StatusEnum.java
@@ -11,19 +11,22 @@ public enum StatusEnum {
     FALSE(0,"false"),
     TRUE(1,"true");
 
-    /**
-     * 点赞状态码
-     */
     private int code;
-    /**
-     * 点赞状态
-     */
+
     private String name;
 
     public static String GetName(Integer code) {
         for (StatusEnum statusEnum : StatusEnum.values()) {
             if (code.equals(statusEnum.getCode())) {
                 return statusEnum.getName();
+            }
+        }
+        return null;
+    }
+    public static Integer GetCode(String name) {
+        for (StatusEnum statusEnum : StatusEnum.values()) {
+            if (name.equals(statusEnum.getName())) {
+                return statusEnum.getCode();
             }
         }
         return null;
