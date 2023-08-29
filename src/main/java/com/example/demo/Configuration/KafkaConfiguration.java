1f419ebd55c69463a3758890e45210e612513692
tangbodi <tangbodi@gmail.com>
2023-08-28 23:24:17 -0400

    8/28

--- src/main/java/com/example/demo/Configuration/KafkaConfiguration.java
+++ src/main/java/com/example/demo/Configuration/KafkaConfiguration.java
@@ -5,10 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.support.converter.RecordMessageConverter;
 import org.springframework.kafka.support.converter.StringJsonMessageConverter;
 
-@Configuration
-public class KafkaConfiguration {
-    @Bean
-    public RecordMessageConverter jsonConverter() {
-        return new StringJsonMessageConverter();
-    }
-}
+//@Configuration
+//public class KafkaConfiguration {
+//    @Bean
+//    public RecordMessageConverter jsonConverter() {
+//        return new StringJsonMessageConverter();
+//    }
+//}
