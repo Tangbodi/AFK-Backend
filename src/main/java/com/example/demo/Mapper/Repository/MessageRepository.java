7d651152c66e6eb1a77d1ef336633f45a02fceda
tangbodi <tangbodi@gmail.com>
2023-09-05 17:18:09 -0400

    9/5

--- src/main/java/com/example/demo/Mapper/Repository/MessageRepository.java
+++ src/main/java/com/example/demo/Mapper/Repository/MessageRepository.java
@@ -14,6 +14,6 @@ public interface MessageRepository extends JpaRepository<Message,Long> {
 
     @Query(value = "SELECT m.comment_reply_id, m.content, m.from_uid, m.to_uid, ui.username AS from_username, m.created_at FROM afk.messages m\n" +
             "JOIN afk.users_info ui ON m.from_uid = ui.user_id\n" +
-            "WHERE m.to_uid = :userId", nativeQuery = true)
+            "WHERE m.to_uid = :userId ORDER BY m.created_at DESC", nativeQuery = true)
     List<Map<Short, Object>> getMessageHistoryByUserId(@Param("userId") Long userId);
 }
