9d550e5e64d91df1490049d0efe7158e53794398
tangbodi <tangbodi@gmail.com>
2023-09-12 09:52:10 -0400

    9/12

--- src/main/java/com/example/demo/Service/Posts/PostImageService.java
+++ src/main/java/com/example/demo/Service/Posts/PostImageService.java
@@ -34,7 +34,6 @@ public class PostImageService {
     @Autowired
     private PostImageRepository postImageRepository;
 
-    @Async("MultiExecutor")
     @Transactional(rollbackOn = Exception.class)
     public void SavePostImage(PostDTO postDTO) {
         logger.info("Saving PostImage: {}");
@@ -92,7 +91,7 @@ public class PostImageService {
                     Path nginxImagePath  = Paths.get(NGINX_POST_IMAGE_PATH, imageName);
                     Thumbnails.of(new ByteArrayInputStream(imageData))
                             .size(800, 600) // Set your desired resolution here
-                            .outputQuality(0.5) // Adjust quality (0.0 to 1.0)
+                            .outputQuality(1.0) // Adjust quality (0.0 to 1.0)
                             .toFile(nginxImagePath.toFile());
                     // Save image to Tomcat and Nginx
 //                    FileOutputStream fos_tomcat = new FileOutputStream(tomcatImagePath.toFile());
