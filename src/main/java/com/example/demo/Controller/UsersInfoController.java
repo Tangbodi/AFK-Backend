e31b119e8bb871605c7d48820951a49b297ee5a4
tangbodi <tangbodi@gmail.com>
2023-10-09 21:11:53 -0400

    10/09

--- src/main/java/com/example/demo/Controller/UsersInfoController.java
+++ src/main/java/com/example/demo/Controller/UsersInfoController.java
@@ -26,7 +26,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -88,7 +87,7 @@ public class UsersInfoController {
 
     }
 
-    @PutMapping(value = "/update-avatar")
+    @PutMapping(value = "/update-avatar",consumes = "multipart/form-data",produces = "application/json")
     public ResponseEntity UpdateUserAvatar(@RequestParam("image") MultipartFile images, HttpServletRequest request) {
         Long userId = (Long) request.getSession().getAttribute("userId");
         ApiResponse apiResponse;
