2035912d6fb895d83e8f66a386cd4724cb7267ec
tangbodi <tangbodi@gmail.com>
2024-06-04 18:38:35 -0400

    6/4

--- src/main/java/com/example/demo/Controller/UpdatePostTotalLike.java
+++ src/main/java/com/example/demo/Controller/UpdatePostTotalLike.java
@@ -31,7 +31,7 @@ public class UpdatePostTotalLike {
     @Autowired
     private PostInfoService postInfoService;
 
-    @Scheduled(fixedRate = 13000)
+    @Scheduled(fixedRate = 15000)
     @PutMapping("/update-post-like")
     public ResponseEntity UpdatePostTotalLike() {
         ApiResponse apiResponse;
